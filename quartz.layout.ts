31d748926f727ad11514a79e57de2880c841ca06
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-04-16 19:04:00 +0200

    Quartz sync: Apr 16, 2025, 7:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/nguterresn/",
-      "Linkedin": "https://www.linkedin.com/in/guterresnogueira/",
+      Linkedin: "https://www.linkedin.com/in/guterresnogueira/",
     },
   }),
 }
@@ -27,12 +27,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    //    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    //  Component.Backlinks(),
   ],
 }
 
